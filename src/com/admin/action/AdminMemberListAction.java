70947eff85ddcb6065de09370dee7069b86c1b4c
babyhaychoi <rularula.autmn@gmail.com>
2022-10-19 16:38:45 +0900

    Revert ","
    This reverts commit 1b2389b3ebe0f0930b4cc6b4109165b02a0edeae, reversing
    changes made to e7543c93e15e9d7d7f77bf41838cda32558ed200.

--- src/com/admin/action/AdminMemberListAction.java
+++ src/com/admin/action/AdminMemberListAction.java
@@ -1,33 +1,21 @@
 package com.admin.action;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.controller.Action;
 import com.controller.ActionForward;
-import com.model.MemberDAO;
-import com.model.MemberDTO;
 
 public class AdminMemberListAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// 전체 레코드를 조회하여 해당 전체 리스트를 view page 로 이동시키는 비지니스 로직.
 		
-		MemberDAO dao = MemberDAO.getInstance();
-		
-		// 목록 조회 리스트 메서드.
-		List<MemberDTO> list = dao.memberList();
-		
-		request.setAttribute("List", list);
-		ActionForward forward = new ActionForward();
-		forward.setRedirect(false);
-		forward.setPath("member/member_list.jsp");
 
-		return forward;
+
+		return null;
 	}
 
 }
