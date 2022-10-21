31be6f53f4c351f9e65ed9ad01a9cf45cb19fb1a
Dongjin, Noh <ndj11111@gmail.com>
2022-10-21 10:25:38 +0900

    DB DTO,DAO 생성

---
+++ src/com/model/ReservDTO.java
@@ -0,0 +1,181 @@
+package com.model;
+
+public class ReservDTO {
+    private int reserv_no;
+    private String reserv_status;
+    private String reserv_sess;
+    private int reserv_stayno;
+    private String reserv_stayname;
+    private int reserv_roomno;
+    private String reserv_roomname;
+    private String reserv_memid;
+    private String reserv_memname;
+    private String reserv_memphone;
+    private String reserv_mememail;
+    private String reserv_start;
+    private String reserv_end;
+    private int reserv_daycount;
+    private int reserv_basic_price;
+    private String reserv_option_name;
+    private int reserv_option_price;
+    private int reserv_use_point;
+    private int reserv_total_price;
+    private int reserv_people_adult;
+    private int reserv_people_kid;
+    private int reserv_people_baby;
+    private String reserv_pickup;
+    private String reserv_request;
+    private int reserv_date;
+
+
+    public int getReserv_no() {
+        return reserv_no;
+    }
+    public void setReserv_no(int reserv_no) {
+        this.reserv_no = reserv_no;
+    }
+    public String getReserv_status() {
+        return reserv_status;
+    }
+    public void setReserv_status(String reserv_status) {
+        this.reserv_status = reserv_status;
+    }
+    public String getReserv_sess() {
+        return reserv_sess;
+    }
+    public void setReserv_sess(String reserv_sess) {
+        this.reserv_sess = reserv_sess;
+    }
+    public int getReserv_stayno() {
+        return reserv_stayno;
+    }
+    public void setReserv_stayno(int reserv_stayno) {
+        this.reserv_stayno = reserv_stayno;
+    }
+    public String getReserv_stayname() {
+        return reserv_stayname;
+    }
+    public void setReserv_stayname(String reserv_stayname) {
+        this.reserv_stayname = reserv_stayname;
+    }
+    public int getReserv_roomno() {
+        return reserv_roomno;
+    }
+    public void setReserv_roomno(int reserv_roomno) {
+        this.reserv_roomno = reserv_roomno;
+    }
+    public String getReserv_roomname() {
+        return reserv_roomname;
+    }
+    public void setReserv_roomname(String reserv_roomname) {
+        this.reserv_roomname = reserv_roomname;
+    }
+    public String getReserv_memid() {
+        return reserv_memid;
+    }
+    public void setReserv_memid(String reserv_memid) {
+        this.reserv_memid = reserv_memid;
+    }
+    public String getReserv_memname() {
+        return reserv_memname;
+    }
+    public void setReserv_memname(String reserv_memname) {
+        this.reserv_memname = reserv_memname;
+    }
+    public String getReserv_memphone() {
+        return reserv_memphone;
+    }
+    public void setReserv_memphone(String reserv_memphone) {
+        this.reserv_memphone = reserv_memphone;
+    }
+    public String getReserv_mememail() {
+        return reserv_mememail;
+    }
+    public void setReserv_mememail(String reserv_mememail) {
+        this.reserv_mememail = reserv_mememail;
+    }
+    public String getReserv_start() {
+        return reserv_start;
+    }
+    public void setReserv_start(String reserv_start) {
+        this.reserv_start = reserv_start;
+    }
+    public String getReserv_end() {
+        return reserv_end;
+    }
+    public void setReserv_end(String reserv_end) {
+        this.reserv_end = reserv_end;
+    }
+    public int getReserv_daycount() {
+        return reserv_daycount;
+    }
+    public void setReserv_daycount(int reserv_daycount) {
+        this.reserv_daycount = reserv_daycount;
+    }
+    public int getReserv_basic_price() {
+        return reserv_basic_price;
+    }
+    public void setReserv_basic_price(int reserv_basic_price) {
+        this.reserv_basic_price = reserv_basic_price;
+    }
+    public String getReserv_option_name() {
+        return reserv_option_name;
+    }
+    public void setReserv_option_name(String reserv_option_name) {
+        this.reserv_option_name = reserv_option_name;
+    }
+    public int getReserv_option_price() {
+        return reserv_option_price;
+    }
+    public void setReserv_option_price(int reserv_option_price) {
+        this.reserv_option_price = reserv_option_price;
+    }
+    public int getReserv_use_point() {
+        return reserv_use_point;
+    }
+    public void setReserv_use_point(int reserv_use_point) {
+        this.reserv_use_point = reserv_use_point;
+    }
+    public int getReserv_total_price() {
+        return reserv_total_price;
+    }
+    public void setReserv_total_price(int reserv_total_price) {
+        this.reserv_total_price = reserv_total_price;
+    }
+    public int getReserv_people_adult() {
+        return reserv_people_adult;
+    }
+    public void setReserv_people_adult(int reserv_people_adult) {
+        this.reserv_people_adult = reserv_people_adult;
+    }
+    public int getReserv_people_kid() {
+        return reserv_people_kid;
+    }
+    public void setReserv_people_kid(int reserv_people_kid) {
+        this.reserv_people_kid = reserv_people_kid;
+    }
+    public int getReserv_people_baby() {
+        return reserv_people_baby;
+    }
+    public void setReserv_people_baby(int reserv_people_baby) {
+        this.reserv_people_baby = reserv_people_baby;
+    }
+    public String getReserv_pickup() {
+        return reserv_pickup;
+    }
+    public void setReserv_pickup(String reserv_pickup) {
+        this.reserv_pickup = reserv_pickup;
+    }
+    public String getReserv_request() {
+        return reserv_request;
+    }
+    public void setReserv_request(String reserv_request) {
+        this.reserv_request = reserv_request;
+    }
+    public int getReserv_date() {
+        return reserv_date;
+    }
+    public void setReserv_date(int reserv_date) {
+        this.reserv_date = reserv_date;
+    }
+}
