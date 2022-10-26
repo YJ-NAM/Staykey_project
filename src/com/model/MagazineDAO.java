ebb948925c3f742b8dcf7d961371d8dfe184784b
bbbmore <icebear0425@naver.com>
2022-10-26 14:05:09 +0900

    dao안뜸
    리스트안뜸~~

--- src/com/model/MagazineDAO.java
+++ src/com/model/MagazineDAO.java
@@ -3,13 +3,14 @@ package com.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-
-
 
 
 public class MagazineDAO {
@@ -107,10 +108,91 @@ public class MagazineDAO {
 
 
     // ======================================================
-    // 
+    // 매거진 목록 메서드
     // ======================================================
 
+    	public List<MagazineDTO> magazineList(int page, int rowsize, Map<String, Object> map) {
+        List<MagazineDTO> list = new ArrayList<MagazineDTO>();
 
+        int startNo = (page * rowsize) - (rowsize - 1);
+        int endNo = (page * rowsize);
+
+        // 검색용 설정
+        String search_sql1 = " where bbs_no > 0";
+        String search_sql2 = "";
+
+        
+        if (map.get("mg_stayno") != "" && map.get("mg_stayno") != null) {
+            search_sql2 += " and bbs_no like '%" + map.get("mg_stayno") + "%'";
+        }
+        if (map.get("mg_date") != "" && map.get("mg_date") != null) {
+            search_sql2 += " and bbs_date like '%" + map.get("mg_date") + "%'";
+        }
+        if (map.get("mg_title") != "" && map.get("mg_title") != null) {
+            search_sql2 += " and bbs_title like '%" + map.get("mg_title") + "%'";
+        }
+
+        search_sql1 += search_sql2;
+
+        
+        
+        
+        // 정렬용 설정
+        String order_sql = "bbs_date";
+        
+        if (map.get("ps_order").equals("bbs_date_desc")) {
+            order_sql = "bbs_date desc";
+        } else if (map.get("ps_order").equals("bbs_date_asc")) {
+            order_sql = "bbs_date asc";
+        } else if (map.get("ps_order").equals("bbs_stayno_desc")) {
+            order_sql = "bbs_stayno desc";
+        } else if (map.get("ps_order").equals("bbs_stayno_asc")) {
+            order_sql = "bbs_stayno asc";
+        } 
+
+        try {
+            openConn();
+            
+            sql = "select * from (select row_number() over(order by " + order_sql
+                    + ") rnum, b.* from staykey_magazine b" + search_sql1 + ") where rnum >= ? and rnum <= ?" + search_sql2;
+            
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, startNo);
+            pstmt.setInt(2, endNo);
+            rs = pstmt.executeQuery();
+            
+            while(rs.next()) {
+            	System.out.println("여기?");
+                MagazineDTO dto = new MagazineDTO();
+
+                dto.setBbs_no(rs.getInt("bbs_no"));
+                dto.setBbs_title(rs.getString("bbs_title"));
+                dto.setBbs_content(rs.getString("bbs_content"));
+                dto.setBbs_file1(rs.getString("bbs_file1"));
+                dto.setBbs_file2(rs.getString("bbs_file2"));
+                dto.setBbs_file3(rs.getString("bbs_file3"));
+                dto.setBbs_file4(rs.getString("bbs_file4"));
+                dto.setBbs_file5(rs.getString("bbs_file5"));
+                dto.setBbs_stayno(rs.getString("bbs_stayno"));
+                dto.setBbs_hit(rs.getInt("bbs_hit"));
+                dto.setBbs_writer_name(rs.getString("bbs_writer_name"));
+                dto.setBbs_writer_id(rs.getString("bbs_writer_id"));
+                dto.setBbs_writer_pw(rs.getString("bbs_writer_pw"));
+                dto.setBbs_date(rs.getString("bbs_date"));
+                
+                
+                list.add(dto);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            closeConn(rs, pstmt, con);
+        }
+
+        return list;
+    }
 
 
 
