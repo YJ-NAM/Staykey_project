e6bf948b7b388a86b65397e1a259f7436b4fc9f9
YJ-Nam <107021486+YJ-NAM@users.noreply.github.com>
2022-10-27 11:44:31 +0900

    stay_room_modify.jsp 완성
    stay_room_modify.jsp 완성

--- src/com/admin/action/AdminStayRoomModifyAction.java
+++ src/com/admin/action/AdminStayRoomModifyAction.java
@@ -7,13 +7,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.controller.Action;
 import com.controller.ActionForward;
+import com.model.StayDAO;
+import com.model.StayRoomDTO;
 
 public class AdminStayRoomModifyAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		// room 수정하기 방 정보 받아오기
+		
+		int roomNo = Integer.parseInt(request.getParameter("room_no"));
+		int stayNo = Integer.parseInt(request.getParameter("stay_no")); // 필요할 시 사용 예정...
+		
+		StayDAO dao = StayDAO.getInstance();
+		StayRoomDTO dto = dao.getStayRoomView(roomNo);
+		
+		request.setAttribute("roomModify", dto);
+		
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("stay/stay_room_modify.jsp");
+		
+		return forward;
 	}
 
 }
