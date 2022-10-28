6c78a0227d44371da95e771b3713f8efd70d5443
bbbmore <icebear0425@naver.com>
2022-10-28 17:50:46 +0900

    1028
    1028

--- src/com/admin/action/AdminMagazineWriteAction.java
+++ src/com/admin/action/AdminMagazineWriteAction.java
@@ -23,16 +23,37 @@ public class AdminMagazineWriteAction implements Action {
 		// write 버튼 누르면 stay 정보가 write로 넘어감.
 
 		MagazineDTO dto = new MagazineDTO();
+		
         StayDAO sdao = StayDAO.getInstance();
         List<StayDTO> slist = sdao.getBbsStayList(dto.getBbs_stayno());
         request.setAttribute("stayList", slist);
 
+
+        
+        // 뷰에 전달할 매개변수 저장용 맵 생성
+        Map<String, Object> map = new HashMap<String, Object>();
+
+
+        // 검색용 변수들 정의
+        String mg_title = "";
+        String mg_writer_name = "";
+        String ps_order = "";
+
+        if(request.getParameter("mg_title") != null){ mg_title = request.getParameter("mg_title").trim(); }else{ mg_title = ""; }
+        if(request.getParameter("mg_writer_name") != null){ mg_writer_name = request.getParameter("mg_writer_name").trim(); }else{ mg_writer_name = ""; }
+        if(request.getParameter("ps_order") != null){ ps_order = request.getParameter("ps_order").trim(); }else{ ps_order = "bbs_date_desc"; }
+
+
+        
         
     	ActionForward forward = new ActionForward();
     	forward.setRedirect(false);
     	forward.setPath("magazine/magazine_write.jsp");
+    	System.out.println("slist" + slist);
 
+    	
         return forward;
+        
 		
 		
 	}
