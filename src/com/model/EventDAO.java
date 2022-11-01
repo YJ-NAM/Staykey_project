61f4f739e4c69be62bb8105cd151242c34085632
ohhyunrok <111178599+ohhyunrok@users.noreply.github.com>
2022-11-01 15:05:14 +0900

    .

--- src/com/model/EventDAO.java
+++ src/com/model/EventDAO.java
@@ -71,6 +71,7 @@ public class EventDAO {
 		}
 	}
 
+
 	// ======================================================
 	// DB 전체 데이터 갯수 메서드
 	// ======================================================
@@ -79,9 +80,26 @@ public class EventDAO {
 
 		// 검색용 설정
 		String search_sql = " where bbs_no > 0";
+		
+		if (map.get("ps_name") != null) {
+			search_sql += " and bbs_writer_name like '%" + map.get("ps_name") + "%'";
+		}
+		if (map.get("ps_id") != null) {
+			search_sql += " and bbs_writer_id like '%" + map.get("ps_id") + "%'";
+		}
 		if (map.get("ps_title") != null) {
 			search_sql += " and bbs_title like '%" + map.get("ps_title") + "%'";
 		}
+		
+        if(!map.get("ps_duse").equals("1")) {
+            String sql_start_date = (String)map.get("ps_start");
+                   sql_start_date = sql_start_date.replace("-", "");
+            String sql_end_date = (String)map.get("ps_end");
+                   sql_end_date = sql_end_date.replace("-", "");
+            search_sql += " and ( (to_char(bbs_showstart, 'YYYYMMDD') >= " + sql_start_date + " and to_char(bbs_showstart, 'YYYYMMDD') <= " + sql_end_date + ")";
+            search_sql += " or (to_char(bbs_showend, 'YYYYMMDD') >= " + sql_start_date + " and to_char(bbs_showend, 'YYYYMMDD') <= " + sql_end_date + ") )";
+        }
+
 
 		try {
 			openConn();
@@ -118,14 +136,24 @@ public class EventDAO {
 		String search_sql2 = "";
 
 		if (map.get("ps_name") != "" && map.get("ps_name") != null) {
-			search_sql2 += " and member_name like '%" + map.get("ps_name") + "%'";
+			search_sql2 += " and bbs_writer_name like '%" + map.get("ps_name") + "%'";
 		}
 		if (map.get("ps_id") != "" && map.get("ps_id") != null) {
-			search_sql2 += " and member_id like '%" + map.get("ps_id") + "%'";
+			search_sql2 += " and bbs_writer_id like '%" + map.get("ps_id") + "%'";
 		}
 		if (map.get("ps_title") != "" && map.get("ps_title") != null) {
-			search_sql2 += " and bbs_title like '%" + map.get("ps_email") + "%'";
+			search_sql2 += " and bbs_title like '%" + map.get("ps_title") + "%'";
 		}
+		
+        if(!map.get("ps_duse").equals("1")) {
+            String sql_start_date = (String)map.get("ps_start");
+                   sql_start_date = sql_start_date.replace("-", "");
+            String sql_end_date = (String)map.get("ps_end");
+                   sql_end_date = sql_end_date.replace("-", "");
+            search_sql2 += " and ( (to_char(bbs_showstart, 'YYYYMMDD') >= " + sql_start_date + " and to_char(bbs_showstart, 'YYYYMMDD') <= " + sql_end_date + ")";
+            search_sql2 += " or (to_char(bbs_showend, 'YYYYMMDD') >= " + sql_start_date + " and to_char(bbs_showend, 'YYYYMMDD') <= " + sql_end_date + ") )";
+        }
+
 
 		search_sql1 += search_sql2;
 
@@ -144,9 +172,9 @@ public class EventDAO {
 		} else if (map.get("ps_order").equals("hit_asc")) {
 			order_sql = "bbs_hit asc";
 		} else if (map.get("ps_order").equals("show_desc")) {
-			order_sql = "bbs_showend desc";
+			order_sql = "bbs_showstart desc";
 		} else if (map.get("ps_order").equals("show_asc")) {
-			order_sql = "bbs_showend asc";
+			order_sql = "bbs_showstart asc";
 		}
 
 		try {
