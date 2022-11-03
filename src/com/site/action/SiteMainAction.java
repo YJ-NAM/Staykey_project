497cf48d58d018bdf4eff1066d6465326a1aa954
YJ Nam <107021486+YJ-NAM@users.noreply.github.com>
2022-11-04 07:58:52 +0900

    Update SiteMainAction.java

--- src/com/site/action/SiteMainAction.java
+++ src/com/site/action/SiteMainAction.java
@@ -1,6 +1,8 @@
 package com.site.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,7 +36,7 @@ public class SiteMainAction implements Action {
     	int randomNum = 0;
     	
     	// display : 메인에 표시할 총 개수 
-    	int[] display = new int[5];
+    	int[] display = new int[10];
     	
     	// stayNums[] : 숙소 번호 담겨 있음
     	int[] stayNums = stayDAO.getStayNums(stayTotal);
@@ -55,36 +57,41 @@ public class SiteMainAction implements Action {
     	
     	// 이벤트 번호에 따른 숙소 이름 조회
     	int bbs_no = 0;
-    	String eventStayNums = "";
-    	String[] eventNums = null;
-    	String[] stayName = null;
-    	int[] stayIntName = null;
+    	String[] eventStayNum = new String[eventList.size()];
+    	int[] stayIntNums = null;
     	
-    	// 모든 이벤트의 bbs_no(이벤트 번호)에 해당하는 숙소 번호값 추출
+    	List<String> eventStayList = new ArrayList<String>();
+    	
+    	// total event => 모든 bbs_no(이벤트 번호)에 해당하는 숙소 번호값 추출 /String + / 로 합쳐진 상태/
     	for(int i=0; i<eventList.size(); i++) {    		
     		bbs_no = eventList.get(i).getBbs_no();
-    		eventStayNums = eventDAO.getEventStayNums(bbs_no);
+    		eventStayNum[i] = eventDAO.getEventStayNums(bbs_no);
+    		System.out.println(eventStayNum[i]);
+    		String[] eventNums = eventStayNum[i].split("/");
+    		System.out.println("여기"+eventNums.length);
+    		for(int j=0; j<eventNums.length; j++) {
+    			eventStayList.add(eventNums[j]);
+    			System.out.println(eventStayList.get(i));
+    		}
     	}
+//		/*
+//		 * System.out.println(eventNums.length);
+//		 *///    	eventStayList.add(e)
+//    	// 숙소 번호 split해서 해당하는 값 int로 형변환 후 getStayView 메서드 실행
+//    	// getStay_name으로 숙소명 추출 -> list에 해당하는 모든 값 add
+//    	for(int i=0; i<eventNums.length; i++) {
+//    		System.out.println(eventNums[i]);
+////    		stayIntNums[i] = Integer.parseInt(eventNums[i]);			 
+//    	}
+//    	stayIntNums = new int[eventNums.length];
+//    	
+//    	
+//    	stayName[i] =
+//    			stayDAO.getStayView(stayIntNums[i]).getStay_name();
+//    	System.out.println("현록이변심"+stayName[i]);
+//    	
     	
-    	// 숙소 번호 split해서 해당하는 값 int로 형변환 후 getStayView 메서드 실행
-    	// getStay_name으로 숙소명 추출 -> list에 해당하는 모든 값 add
-    	eventNums = eventStayNums.split("/");
-    	stayName = new String[eventNums.length];
-    	System.out.println();
-    	for(int i=0; i<eventNums.length; i++) {
-    		System.out.println(eventNums[i]);
-    		stayIntName[i] = Integer.parseInt(eventNums[i]);
-    		System.out.println(stayIntName[i]);
-			/*
-			 * stayName[i] =
-			 * stayDAO.getStayView(Integer.parseInt(eventNums[i])).getStay_name();
-			 * System.out.println("현록이변심"+stayName[i]);
-			 */
-    	}
-    	
-    	
-    	
-    	request.setAttribute("stayName", stayName);
+//    	request.setAttribute("stayName", stayName);
     	request.setAttribute("eventList", eventList);
     
     	
